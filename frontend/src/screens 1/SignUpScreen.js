0ea79f401890db787d70909bd556a0e74b3ab3c8
Ismail Belhoula <belhoulaismail@fsm.u-monastir.tn>
2024-05-10 18:58:53 +0100

    testé

--- frontend/src/screens 1/SignUpScreen.js
+++ frontend/src/screens 1/SignUpScreen.js
@@ -41,7 +41,13 @@ const SignUpScreen = ({ navigation }) => {
           source={require("../../assets/logo.png")}
           style={{ width: 150, height: 150, alignSelf: "center" }}
         />
-
+        <Input
+          placeholder="Email"
+          containerStyle={{ marginTop: 10, width: 300 }}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          leftIcon={<Icon name="envelope" size={16} />}
+        />
         <Input
           placeholder="Password"
           containerStyle={{ marginTop: 10, width: 300 }}
