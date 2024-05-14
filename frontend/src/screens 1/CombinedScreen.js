79f9cc100c655e1b791ff2c6bbce09e6c5474fbf
Ismail Belhoula <belhoulaismail@fsm.u-monastir.tn>
2024-05-14 20:35:45 +0100

    added 3 button that tkae products's photo

--- frontend/src/screens 1/CombinedScreen.js
+++ frontend/src/screens 1/CombinedScreen.js
@@ -14,6 +14,7 @@ import * as Camera from "expo-camera";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Checkbox from "expo-checkbox"; // You may need to install expo-checkbox
 
 const Stack = createStackNavigator();
 
@@ -27,6 +28,13 @@ function HomeScreen({ navigation, route }) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [isScanning, setIsScanning] = useState(false);
+
+  const [image1, setImage1] = useState(null);
+  const [image2, setImage2] = useState(null);
+  const [image3, setImage3] = useState(null);
+  const [checked1, setChecked1] = useState(false);
+  const [checked2, setChecked2] = useState(false);
+  const [checked3, setChecked3] = useState(false);
 
   const scanBarcode = () => {
     setScanned(false);
@@ -84,6 +92,27 @@ function HomeScreen({ navigation, route }) {
     }
   };
 
+  const selectImage = async (setImage, setChecked) => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    if (status !== "granted") {
+      alert("Sorry, we need camera permissions to make this work!");
+      return;
+    }
+
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.5,
+    });
+
+    if (!result.cancelled) {
+      let uri = result.assets[0].uri;
+      setImage(uri);
+      setChecked(true);
+    }
+  };
+
   const uploadImage = async (uri) => {
     if (!uri) {
       console.log("No URI provided");
@@ -93,7 +122,7 @@ function HomeScreen({ navigation, route }) {
     setLoading(true);
 
     // Replace the URL with your actual endpoint
-    let responseFetch = await fetch("http://192.168.0.3:8080/extract_text", {
+    let responseFetch = await fetch("http://192.168.0.5:8080/extract_text", {
       method: "POST",
       body: createFormData(uri),
       headers: {
@@ -139,7 +168,17 @@ function HomeScreen({ navigation, route }) {
   }
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+        paddingBottom: 30,
+      }}
+    >
+      <Text style={{ alignItems: "flex-start", justifyContent: "flex-start" }}>
+        Take a photo of the product nutation table :{" "}
+      </Text>
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           style={styles.buttons}
@@ -155,25 +194,64 @@ function HomeScreen({ navigation, route }) {
         </TouchableOpacity>
       </View>
       <Text>This is the extracted text: {extractedText}</Text>
+      <Text>Scan the product's Barcode :</Text>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.buttons} onPress={scanBarcode}>
           <Text>Scan Barcode</Text>
         </TouchableOpacity>
       </View>
+      <Text>this is the extracted Barcode :{barcodeData}</Text>
       {isScanning && (
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
           style={StyleSheet.absoluteFillObject}
         />
       )}
-      {scanned && (
-        <View style={{ marginTop: 20 }}>
-          <Image
-            source={{ uri: barcodeData }} // Assuming barcodeData holds the URI of the barcode image
-            style={{ width: 200, height: 200 }}
+      {scanned}
+      <View style={styles.photoButtonContainer}>
+        <View style={styles.photoButtonWrapper}>
+          <TouchableOpacity
+            style={styles.buttons2}
+            onPress={() => selectImage(setImage1, setChecked1)}
+          >
+            <Text>Take Photo 1</Text>
+          </TouchableOpacity>
+          <Checkbox
+            style={styles.checkbox}
+            value={checked1}
+            onValueChange={setChecked1}
           />
+          {image1 && <Image source={{ uri: image1 }} style={styles.image} />}
         </View>
-      )}
+        <View style={styles.photoButtonWrapper}>
+          <TouchableOpacity
+            style={styles.buttons2}
+            onPress={() => selectImage(setImage2, setChecked2)}
+          >
+            <Text>Take Photo 2</Text>
+          </TouchableOpacity>
+          <Checkbox
+            style={styles.checkbox}
+            value={checked2}
+            onValueChange={setChecked2}
+          />
+          {image2 && <Image source={{ uri: image2 }} style={styles.image} />}
+        </View>
+        <View style={styles.photoButtonWrapper}>
+          <TouchableOpacity
+            style={styles.buttons2}
+            onPress={() => selectImage(setImage3, setChecked3)}
+          >
+            <Text>Take Photo 3</Text>
+          </TouchableOpacity>
+          <Checkbox
+            style={styles.checkbox}
+            value={checked3}
+            onValueChange={setChecked3}
+          />
+          {image3 && <Image source={{ uri: image3 }} style={styles.image} />}
+        </View>
+      </View>
     </View>
   );
 }
@@ -239,9 +317,31 @@ const styles = StyleSheet.create({
     shadowRadius: 15,
     elevation: 8,
   },
+  buttons2: {
+    width: 100,
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#fff",
+    margin: 20,
+    height: 55,
+    textAlign: "center",
+    borderRadius: 50,
+    overflow: "hidden",
+    backgroundColor: "#25aae1",
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#4184ea",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.75,
+    shadowRadius: 15,
+    elevation: 8,
+  },
   buttonContainer: {
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     width: "100%",
     paddingHorizontal: 20,
   },
@@ -252,5 +352,35 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingRight: 10,
     borderRadius: 20, // This will make the TextInput circular
+  },
+  photoButtonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "flex-starts",
+    width: "100%",
+    paddingHorizontal: 0,
+    marginTop: 0,
+  },
+  photoButtonWrapper: {
+    flexDirection: "column",
+    alignItems: "center",
+    marginHorizontal: 0,
+  },
+  checkbox: {
+    marginTop: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginTop: 10,
+  },
+  extractedText: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  buttonsContainer2: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
   },
 });
